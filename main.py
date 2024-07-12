d009cca4f3b812ccb854377e4891ccee2113d967
Corvus <corvus_2001@mail.ru>
2024-07-12 11:25:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+my_string = input("Здравствуйте! Как Вас зовут? ") #Введите Ваше имя
+print(my_string.upper())
+print(my_string.lower())
+name = "Хороший день, не правда ли?"
+print(name.replace(" ", ""))
+print(name [0])
+print(name[-1])
